91b62fbb9c8dfa7d896bee5e96ffcb999f511a93
Fatima-ezzahra AKHMASSI <akhmassi.fati.ez@gmail.com>
2022-09-19 08:37:22 +0100

    Last modif

--- src/main/java/com/example/demoFirstApp/DemoFirstAppApplication.java
+++ src/main/java/com/example/demoFirstApp/DemoFirstAppApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,13 +28,16 @@ public class DemoFirstAppApplication implements CommandLineRunner {
 	@Autowired
 	CommentaireRepository commentaireRepository;
 	//Date.from(LocalDate.of(1992, 9, 2).atStartOfDay(ZoneId.systemDefault()).toInstant()))
-
+	@Autowired
+	private RepositoryRestConfiguration restConfiguration;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoFirstAppApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		restConfiguration.exposeIdsFor(Profile.class);
+
 		Date d=new Date();
 		d.setDate(2);
 		d.setMonth(9);
